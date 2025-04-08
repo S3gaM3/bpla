f22186cb95e5b3c35ab4d4b5bc8c45a8586c8918
Kibalnik Sergey <kiborgik87@gmail.com>
2025-04-08 21:58:05 +0300

    first_step

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'bpl6084916_bplabase';
+$CFG->dbuser    = 'bpl6084916_mysql';
+$CFG->dbpass    = 'vL5:HJxX';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_0900_ai_ci',
+);
+
+$CFG->wwwroot   = 'http://bpla-dosaaf.ru';
+$CFG->dataroot  = '/home/bpl6084916/bpla-dosaaf.ru/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
